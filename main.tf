1231aa3dcfb4b4bafeb874de7937eeb89f30040f
himavanthkj <38948602+himavanthkj@users.noreply.github.com>
2021-07-12 13:27:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.prod_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
